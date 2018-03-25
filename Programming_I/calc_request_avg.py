00c07dcc77bd0c27cebc3b3524b7ac566d9bedb2
Charles A. Hessifer <hessifer@gmail.com>
2018-03-24 20:31:34 -0400

    initial commit

---
+++ Programming_I/calc_request_avg.py
@@ -0,0 +1,70 @@
+import datetime
+
+# parse logfile entries and return id, status, milestone, date_time_stamp
+def calc_avg_task_time(file_handle):
+    # separate each line by spaces and capture id, status, milestone
+    # and date_time_stamp and return avg seconds for tasks
+    data_dict = {}
+
+    # td_total (time delta)
+    td_total = 0
+
+    for line in file_handle:
+        # split line by spaces
+        entry = line.strip().split(' ')
+
+        # capture id
+        id = entry[3].split('=')[1]
+
+        # capture status
+        status = entry[-1].split('=')[1]
+
+        # capture milestone
+        milestone = entry[4].split('=')[1]
+
+        # capture date/time stamp for log entry
+        date_time_stamp = entry[0]
+
+        # if status is 200 process data
+        if status == '200':
+            if id not in data_dict and milestone == 'start':
+                # capture report_id and store in data_dict with
+                # appropriate key/values
+                data_dict[id] = {}
+                data_dict[id].update(time_start=date_time_stamp)
+            elif id in data_dict and milestone == 'end':
+                # just update end milestone data in data_dict
+                data_dict[id].update(time_end=date_time_stamp)
+            else:
+                pass
+        # calculate and update time delta in seconds to data_dict
+        # keep track of the total time delta in seconds
+        for key, value in data_dict.items():
+            t_start = datetime.datetime.strptime(value.get(
+                'time_start'), "%Y-%m-%dT%H:%M:%S.%fZ")
+            t_stop = datetime.datetime.strptime(value.get(
+                'time_end'), "%Y-%m-%dT%H:%M:%S.%fZ")
+            td = t_stop - t_start
+            ts = td.total_seconds()
+            # no need to store this if we do not keep it around
+            # data_dict[key].update(td=ts)
+            td_total += ts
+        return td_total / len(data_dict.keys())
+
+    # TODO: add argparse functionality so that user can
+    # specify logfile.
+    # TODO: make sure we support the log file
+
+    # main function
+    def main():
+        msg = "Average time to complete a successful operation: {} seconds."
+
+        # read each line of the file into a buffer
+        with open('logfile.txt', 'r') as fh:
+            data = fh.readlines()
+
+        print(msg.format(calc_avg_task_time(data)))
+
+
+if __name__ == '__main__':
+    main()
