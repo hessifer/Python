9de3f98ad1a79a4a6b058df5b2a17e25ec21da0b
Charles A. Hessifer <hessifer@gmail.com>
2019-08-31 18:18:36 -0400

    initial

---
+++ python_learning_lab/classes/dog.py
@@ -0,0 +1,22 @@
+class Dog:
+    """A simple attempt to model a dog."""
+
+    def __init__(self, name, age):
+        """Initialize name and age attributes."""
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        """Simulate a dog sitting in response to a command."""
+        print(f"{self.name} is now sitting.")
+
+    def roll_over(self):
+        """Simulate rolling over in response to a command."""
+        print(f"{self.name} rolled over!")
+
+
+if __name__ == '__main__':
+    dog = Dog('Blanco', 4)
+    dog.sit()
+    dog.roll_over()
+    dog.sit()
