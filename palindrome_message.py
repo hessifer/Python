07cbfe27eb3971e3f383b27ee8bd35bfd1593050
Charles A. Hessifer <hessifer@gmail.com>
2020-03-14 23:35:05 -0400

    working on learning notebooks

--- palindrome_message.py
+++ palindrome_message.py
@@ -1,7 +1,7 @@
 """Checks if enter message is a palindrome"""
 
-def main():
-    message = input('Enter a message: ').lower()
+
+def is_palindrome(message):
     message_clean = message
 
     # remove unwanted spaces and punctuations
@@ -9,7 +9,20 @@ def main():
         message_clean = message_clean.replace(c, "")
 
     if message_clean == message_clean[::-1]:
+        return True
+    return False
+
+
+def main():
+    message = input('Enter a message: ').lower()
+
+    if is_palindrome(message):
         print('The message is a palindrome.')
+    else:
+        print('The message is not a palindrome.')
+
 
 if __name__ == '__main__':
-    main()
+    import timeit
+    print(timeit.timeit('is_palindrome("bob")', setup="from __main__ import is_palindrome"))
+    # main()
