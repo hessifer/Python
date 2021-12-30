3feead9a07ea3475fc7fa6ea88e5a5b4bc209d5b
Charles A. Hessifer <hessifer@gmail.com>
2021-12-29 22:01:01 -0600

    more practice exercises

---
+++ UdacityDataStructures/diff_date_days.py
@@ -0,0 +1,105 @@
+# Credit goes to Websten from forums
+#
+# Program defensively:
+#
+# What do you do if your input is invalid? For example what should
+# happen when date 1 is not before date 2?
+#
+# Add an assertion to the code for daysBetweenDates to give
+# an assertion failure when the inputs are invalid. This should
+# occur when the first date is not before the second date.
+#
+
+def isLeapYear(year):
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
+
+
+def daysInMonth(year, month):
+    if month == 2:
+        if isLeapYear(year):
+            return 29
+        else:
+            return 28
+    elif month in (1, 3, 5, 7, 8, 10, 12):
+        return 31
+    else:
+        return 30
+
+
+def nextDay(year, month, day):
+    """Simple version: assume every month has 30 days"""
+    if day < daysInMonth(year, month):
+        return year, month, day + 1
+    else:
+        if month == 12:
+            return year + 1, 1, 1
+        else:
+            return year, month + 1, 1
+
+
+def dateIsBefore(year1, month1, day1, year2, month2, day2):
+    """Returns True if year1-month1-day1 is before
+       year2-month2-day2. Otherwise, returns False."""
+    if year1 < year2:
+        return True
+    if year1 == year2:
+        if month1 < month2:
+            return True
+        if month1 == month2:
+            return day1 < day2
+    return False
+
+
+def daysBetweenDates(year1, month1, day1, year2, month2, day2):
+    """Returns the number of days between year1/month1/day1
+       and year2/month2/day2. Assumes inputs are valid dates
+       in Gregorian calendar."""
+    # program defensively! Add an assertion if the input is not valid!
+    assert not dateIsBefore(year2, month2, day2, year1, month1, day1)
+    days = 0
+    while dateIsBefore(year1, month1, day1, year2, month2, day2):
+        year1, month1, day1 = nextDay(year1, month1, day1)
+        days += 1
+    return days
+
+
+def my_tests():
+    # tests with 30 days month
+    print("Initiating tests....")
+    assert daysBetweenDates(2013, 1, 1, 2013, 1, 1) == 0
+    assert daysBetweenDates(2013, 1, 1, 2013, 1, 2) == 1
+    assert nextDay(2013, 1, 1) == (2013, 1, 2)
+    assert nextDay(2013, 4, 30) == (2013, 5, 1)
+    assert nextDay(2012, 12, 31) == (2013, 1, 1)
+    assert nextDay(2013, 2, 28) == (2013, 3, 1)
+    assert nextDay(2013, 9, 30) == (2013, 10, 1)
+    assert nextDay(2012, 2, 28) == (2012, 2, 29)
+    assert daysBetweenDates(2013, 1, 1, 2014, 1, 1) == 365
+    assert daysBetweenDates(2012, 1, 1, 2013, 1, 1) == 366
+    print("All tests have completed!")
+
+
+def tests():
+    test_cases = [((2012, 1, 1, 2012, 2, 28), 58),
+                  ((2012, 1, 1, 2012, 3, 1), 60),
+                  ((2011, 6, 30, 2012, 6, 30), 366),
+                  ((2011, 1, 1, 2012, 8, 8), 585),
+                  ((1900, 1, 1, 1999, 12, 31), 36523)]
+
+    for (args, answer) in test_cases:
+        result = daysBetweenDates(*args)
+        if result != answer:
+            print(f"Test with data: {args} failed")
+        else:
+            print("Test case passed!")
+
+
+tests()
+my_tests()
