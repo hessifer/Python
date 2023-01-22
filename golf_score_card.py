1c433c86ee364e0e300932b4134078edc37a539e
Charles A. Hessifer <hessifer@gmail.com>
2023-01-22 01:43:06 -0600

    some late night fun

--- golf_score_card.py
+++ golf_score_card.py
@@ -1,14 +1,10 @@
 import sys
 import os
+import time
 
 
 def main():
-    # DONE: top level menu to add a round of golf,
-    # DONE: implement option 1
-    # TODO: implement option 2
-    # DONE: implement option 3
     golf_score_book = {'frasch': {'charles': []}}  # course, player, scores
-
     while True:
         clear_screen()
         menu(golf_score_book)
@@ -20,7 +16,7 @@ def menu(score_db):
     menu_option2 = "2.) Calculate Average Round of Golf"
     menu_option3 = "3.) Quit"
     
-    print(f"{menu_banner}\n{menu_option1}\n{menu_option2}\n{menu_option3}\n")
+    print(f"\n{menu_banner}\n{menu_option1}\n{menu_option2}\n{menu_option3}\n")
     answer = int(input("\n> "))
 
     if answer == 3:
@@ -28,8 +24,10 @@ def menu(score_db):
     if answer == 1:
         name = input("Enter Player Name: ")
         add_round(score_db, name)
-    print(f"{score_db}")
-
+    if answer == 2:
+        name = input("Enter Player Name: ")
+        calculate_average_round(score_db, name)
+        time.sleep(3)
 
 def add_round(golf_history, player):
     # golf_history is the dictionary of golf scores
@@ -53,9 +51,23 @@ def add_round(golf_history, player):
 
 
 def calculate_average_round(golf_history, player):
-    # check if 5 rounds have been played
-    pass
+    player = player.lower()
+    # get number of rounds played
+    rounds_played = 0
+    total_score = 0
 
+    for key in golf_history.keys():
+        for k in golf_history[key].keys():
+            if k == player:
+                rounds_played += len(golf_history[key][k])
+                if rounds_played:
+                     total_score += sum(golf_history[key][k])    
+
+    # check if 5 rounds have been played
+    if rounds_played < 5:
+        print(f"{player.title()} has not played enough rounds. ({rounds_played}/5)")
+    else:
+        print(f"Your average score for {rounds_played} rounds played is {int(total_score / rounds_played)}.")
 
 def clear_screen():
     if os.name == 'posix':
