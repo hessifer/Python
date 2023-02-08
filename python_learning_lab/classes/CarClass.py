79161c80ccfa452d9ed510b15591fbe7f69e4395
Charles A. Hessifer <hessifer@gmail.com>
2023-02-07 22:08:13 -0500

    more class testing

---
+++ python_learning_lab/classes/CarClass.py
@@ -0,0 +1,14 @@
+# Create a Car class
+# Keep track of the number of instances of the Car class
+# Create an init / constructor method that sets the following values
+# - color, number_of_doors, make, model
+# Create a ignition instance method that takes a start or stop value
+# Create a brake instance method that slows the car down (let the user know they are slowing down)
+# Create a accelerate instance method that speeds the car up (let the user know they are speeding up)
+
+
+
+
+# Create an instance of your Car class
+# Utilize all the instance methods in your code
+# Display the number of Car instances created
